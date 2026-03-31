f46f2e641523d178f3570c2e05d75ed55725384f
Sharmin <2021szaman@gmail.com>
2026-03-31 11:12:19 -0500

    Moved repo to correct project folder

--- Assets/Scripts/SimplePlayerMovement.cs
+++ Assets/Scripts/SimplePlayerMovement.cs
@@ -6,15 +6,47 @@ public class SimplePlayerMovement : MonoBehaviour
 
     public float moveSpeed;
     public Rigidbody2D theRB;
+    public float jumpForce;
+
+    private bool isGrounded;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+
+    private bool canDoubleJump;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         theRB.linearVelocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.linearVelocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                theRB.linearVelocity = new Vector2(theRB.linearVelocity.x, jumpForce);
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    theRB.linearVelocity = new Vector2(theRB.linearVelocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
+        }
     }
 }
